f2998891d7a759f97dbb7f3eec1c273c5a6020fd
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-08-27 09:52:10 -0400

    Start About page.

--- jaatube-Clone-App/src/About.jsx
+++ jaatube-Clone-App/src/About.jsx
@@ -1,2 +1,24 @@
 import React from "react";
-import "./assets/";
+import "./About.css";
+import Alex from "./assets/alex.jpeg";
+import Jalal from "./assets/jalal.png";
+import Aisha from "./Users/aishakleemoff/10-2/Mod-3/Mod-3-projects/jaaTube-Clone-Project/jaatube-Clone-App/src/assets/aisha.jpeg";
+
+export default function About() {
+  return (
+    <>
+      <div className="aboutContainer">
+        <h1>Our Team</h1>
+        <section className="description">
+          <h2>Description of our Project</h2>
+          <p>
+            In this project our team created a YouTube clone that allows a user
+            to search for videos using the YouTube API. A user can select videos
+            and watch them. This project is a collaborative effort that includes
+            three claamates using our knowledge of design and React.
+          </p>
+        </section>
+      </div>
+    </>
+  );
+}
--- jaatube-Clone-App/src/App.jsx
+++ jaatube-Clone-App/src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "./About";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-<p>Alexander, Aisha, Jalal</p>
-<h2>Delete it ALL</h2>
-<p>Hopefully this pushes</p>
+        <p>Alexander, Aisha, Jalal</p>
+        <h2>Delete it ALL</h2>
+        <p>Hopefully this pushes</p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
