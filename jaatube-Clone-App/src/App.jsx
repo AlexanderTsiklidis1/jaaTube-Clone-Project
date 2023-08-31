230399c4c9b292df6bd871004fe10385707cdf99
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-08-31 19:23:22 -0400

    Fix bugs

--- jaatube-Clone-App/src/App.jsx
+++ jaatube-Clone-App/src/App.jsx
@@ -1,23 +1,9 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-<<<<<<< HEAD
+
 import About from "./About";
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-import "./components/about/About";
-=======
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-=======
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
->>>>>>> f557b07 (Install additional dependancies.)
-import About from "./About";
->>>>>>> 38fe21e (Add About Route.)
->>>>>>> main
 
 function App() {
   const [count, setCount] = useState(0);
@@ -25,20 +11,12 @@ function App() {
   return (
     <>
       <div>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> main
         <Router>
           <Routes>
             <Route path="/about" element={<About />} />
           </Routes>
         </Router>
-<<<<<<< HEAD
-=======
->>>>>>> 38fe21e (Add About Route.)
->>>>>>> main
+
         <p>Alexander, Aisha, Jalal</p>
         <h2>Delete it ALL</h2>
         <p>Hopefully this pushes</p>
