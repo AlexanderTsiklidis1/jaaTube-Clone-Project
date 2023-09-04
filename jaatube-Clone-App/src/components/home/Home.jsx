46c2939abf9978a854bee474e9d9a3c521b0cce9
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-09-04 05:42:10 -0400

    makes some changes to home jsx component

--- jaatube-Clone-App/src/components/home/Home.jsx
+++ jaatube-Clone-App/src/components/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react"
-import {Link} from 'react-router-dom'
+import { Link, useParams, useNavigate } from "react-router-dom";
 import "/Home.css";
 
 export default function Home() {
+  const [video, setVideo] = useState({});
+  const { id } = useParams();
+
+  const navigate = useNavigate();
+  
     return (
         <div>
       <article className="video"></article>
