98df30889f5d3ef55e5b505b2636a63c0d39aab2
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-09-05 01:41:11 -0400

    reformats video listing componenet a bit

--- jaatube-Clone-App/src/components/home/VideoListing.jsx
+++ jaatube-Clone-App/src/components/home/VideoListing.jsx
@@ -3,19 +3,14 @@ import "./VideoListing.css";
 
 export default function VideoListing({ video }) {
     return (
-      <article className="video">
-        <h3 className="title">
-          {/* using the show prop here */}
-          {/* the <Link /> is for a USER to click on */}
-          <Link to={`/video/${video.id}`}>{video.title}</Link>
-        </h3>
-        <p className="description">{video.snippet.description}</p>
-        <aside className="thumbnail">
-          <p>
-            <span>Listed Categories:</span>
+        <article className="video">
+            <h3 className="title">
+                {/* using the show prop here */}
+                {/* the <Link /> is for a USER to click on */}
+                <Link to={`/video/${video.id}`}>{video.title}</Link>
+            </h3>
             <img src={video.snippet.thumbnails.default.url} alt="thumbnail" />
-          </p>
-        </aside>
-      </article>
+            <p className="description">{video.snippet.description}</p>
+        </article>
     );
   }
