e8aa86f429cfcf1b0968a01293aa2eba39e2b2fd
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-09-05 00:42:12 -0400

    finally gets some youtube data into our APP homepage very nice

--- jaatube-Clone-App/src/components/home/Home.jsx
+++ jaatube-Clone-App/src/components/home/Home.jsx
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from "react"
-import { Link, useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react"
 import { getAllVideos } from "../../api/fetch";
 import VideoListing from "./VideoListing";
-
-//import "/Home.css";
+import "./Home.css";
 
 export default function Home() {
 
-  const [videos, setVideos] = useState ([1]);
+  const [videos, setVideos] = useState ([]);
   
     useEffect(() => {
       getAllVideos()
@@ -15,14 +13,16 @@ export default function Home() {
         setVideos(videosJSON)
       })
     },[])
-
+    const youtubeArray = videos.items
+    console.log(videos.items)
     return (
         <div>
           <section className="videos-index-wrapper">
             <h2>Videos</h2>
             <section className="videos-index">
              {console.log(videos)}
-             { (videos.items).map((video) => {
+             
+             { youtubeArray.map((video) => {
               return <VideoListing video = {video} key = {video.id}/>
             })}
             
